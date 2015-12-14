aaba6aac3080b5c50e4179fcf2b9096d023f0ee2
root <root@localhost.localdomain>
2015-12-14 17:50:41 +0200

    partners links updated

---
+++ blocks/partners.php
@@ -0,0 +1,54 @@
+<section id="banners" class="dnpb_block dnpb_partners">
+	<div class="row">
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://naps.gov.ua"><img class="img-responsive1" height="55" src="./banners/NAPN.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://lib.iitta.gov.ua"><img class="img-responsive"  src="./banners/EL_NAPN.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://kno.rada.gov.ua/komosviti/control/uk/index"><img class="img-responsive12" height="35" width="200"  src="./banners/KOMIT_new.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://www.mon.gov.ua"><img class="img-responsive1" height="55"  src="./banners/MON.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://ula.org.ua/ua/"><img class="img-responsive1" height="55"  src="./banners/UBA_new.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://www.polpred.com"><img class="img-responsive1" height="55"  src="./banners/polpred_banner.png" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://kobzar.ua/site/intro"><img class="img-responsive1" height="55"  width="200" src="./banners/Schevchenka_banner.jpg" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+		<div class="col-md-3">
+			<center>
+				<div class="media"><a href="http://nbuviap.gov.ua"><img class="img-responsive1" height="55"  src="./banners/Baner.png" alt=""></a></div>
+			</center>
+			<br>
+		</div>
+	</div>
+</section>
