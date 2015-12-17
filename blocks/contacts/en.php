777aa24880df2d0e784b26035fca6f66fd520084
root <root@localhost.localdomain>
2015-12-17 14:34:07 +0200

    contacts block styles updated, map link label changed

--- blocks/contacts/en.php
+++ blocks/contacts/en.php
@@ -2,5 +2,5 @@
 Kyiv 04060 Ukraine
 <nobr>380 (44) 467-22-14</nobr>
 <a href="mailto:<?=$dnpb_admin_email?>"><?=$dnpb_admin_email?></a>             
-<a href="<?=get_permalink(14)?>"><nobr>Contact</nobr></a>
+<a href="<?=get_permalink(14)?>"><nobr>Library Location Map</nobr></a>
 
