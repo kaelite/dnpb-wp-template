27ae425297843b74602a7678f7ab5364b05ae189
root <root@localhost.localdomain>
2016-01-08 16:06:27 +0200

    Google Analytics code added

--- functions.php
+++ functions.php
@@ -29,6 +29,27 @@ function dnpb_loadscripts(){
 add_action('wp_enqueue_scripts', 'dnpb_loadscripts');
 
 /**
+ * Add Google Analitycs Scripts
+ **/
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { 
+?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72167824-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<?
+} 
+
+
+/**
  * Register sidebar
  **/
 function dnpb_widgets_init(){
