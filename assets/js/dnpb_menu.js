039b1eb83533c58e9b53fe1c8a91d1f0a9cef262
root <root@localhost.localdomain>
2016-02-16 18:54:21 +0200

    Fixed main menu blinking bug in Mozzilas browsers

--- assets/js/dnpb_menu.js
+++ assets/js/dnpb_menu.js
@@ -1,6 +1,12 @@
 jQuery(function() {
 	    console.log( "Dnpb menu script added!" );
 		jQuery('ul.nav.navbar-nav.dnpb_mainmenu li.dropdown').hoverIntent(function(){if(jQuery(this).hasClass('open')){jQuery(this).removeClass('open');}else{jQuery(this).addClass('open');}})
+		jQuery('ul.nav.navbar-nav.dnpb_mainmenu li.dropdown a[href="#"]').click(function(){return false;});
+
+		jQuery('ul.nav.navbar-nav.dnpb_mainmenu li.dropdown a[href!="#"]').click(function(){
+			window.location.href = jQuery(this).attr("href");
+			return false;
+		})
 });
 
 
